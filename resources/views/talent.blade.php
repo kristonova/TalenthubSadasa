24cc313027669af1e7a0db5cce1c99e6bcc76a00
Gus Khamim <amim.opreker22@gmail.com>
2020-01-26 11:35:21 +0700

    Talenthub Sadasa Statis

---
+++ resources/views/talent.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Talent Sadasa</title>
+    <!-- bootstrap v4.0.0 css -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- font-awesome v4.6.3 css -->
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="shortcut icon" href="https://sadasa.id/wp-content/uploads/2019/03/SADASA_favicon32.png" type="image/x-icon">
+    {{-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css"> --}}
+</head>
+<body>
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light static-top">
+  <div class="container">
+    <a class="navbar-brand" href="https://sadasa.id">
+          <img src="//sadasa.id/wp-content/uploads/2019/05/SADASA-Logo-Red-Horizontal-250.png" alt="">
+        </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item mr-3">
+          <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+        </li>
+        <li class="nav-item mr-3">
+          <a class="nav-link" href="{{url('testing')}}">Talent</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br>
+<div class="container">
+  <h1 class="text-center mb-5 mt-3">Talents Sadasa Academy</h1>
+  <div class="card-columns">
+    <div class="card">
+      <a href="">
+        <img class="card-img-top" src="https://dl.airtable.com/.attachments/d981a201762a50d97f7817aa1f98467e/bacbcc25/AisyahShadrina.jpg">
+      </a>
+      {{-- <a href="#">
+      </a> --}}
+      <div class="card-body">
+        <h5 class="card-title">Nama</h5>
+        <h6 class="card-title">Title (Misal : Web Developer)</h6>
+        <ul style="text-align:right;">
+          <i class="fa fa-star" aria-hidden="true"></i>
+          <i class="fa fa-star" aria-hidden="true"></i>
+          <i class="fa fa-star" aria-hidden="true"></i>
+          <i class="fa fa-star" aria-hidden="true"></i>
+          <i class="fa fa-star" aria-hidden="true"></i>
+        </ul>
+        <p class="card-text" style="color: #8a8a8a; font-size: 15px; padding: 10px;
+        border-bottom: 2px solid #d2d2d4; border-top: 2px solid #d2d2d4;">
+          Description of my abilities
+        </p>
+        <footer class="blockquote-footer text-right">Your <cite title="Motto">Motto</cite></footer>
+        {{-- <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i> --}}
+      </div>
+      <div class="card-footer">
+        {{-- <small class="text-muted">Last updated 3 mins ago</small> --}}
+        <a href="#"><img src="https://www.instagram.com/static/images/ico/favicon-192.png/68d99ba29cc8.png" width="30" height="30"></a>
+        &nbsp;
+        <a href="#"><img src="https://static-exp2.licdn.com/sc/h/br/55ggxxse8uyjdh2x78ht3j40q" width="30" height="30"></a>
+      </div>
+    </div>
+  </div>
+</div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
